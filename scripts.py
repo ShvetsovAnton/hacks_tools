58f483d12f1f2b545789efa0aef7854c7372273a
Anton <56098143+ShvetsovAnton@users.noreply.github.com>
2023-02-05 20:24:41 +0400

    Изменил scripts.py
    - Убрал ненужную функцию
    - Сократил запросы к базе
    - Добавил отработку исключений

--- scripts.py
+++ scripts.py
@@ -3,45 +3,53 @@ from datacenter.models import *
 from django.core.exceptions import ObjectDoesNotExist, MultipleObjectsReturned
 
 
-def fix_marks(name_schoolkid):
-    schoolkid = Schoolkid.objects.get(full_name__contains=name_schoolkid)
-    schoolkid_marks = Mark.objects.filter(schoolkid=schoolkid)
-    for mark in schoolkid_marks:
-        mark.points = 5
-        mark.save()
+COMMENDATION_TEXTS = [
+    "Молодец!", "Отлично!", "Хорошо!",
+    "Гораздо лучше, чем я ожидал!",
+    "Ты меня приятно удивил!", "Великолепно!", "Прекрасно!",
+    "Ты меня очень обрадовал!"]
+
+
+def take_schoolkid(name_schoolkid):
+    try:
+        schoolkid = Schoolkid.objects.get(full_name__contains=name_schoolkid)
+        return schoolkid
+    except ObjectDoesNotExist:
+        print("Кажется допущена ошибка при заполнение данных, \n"
+              "Проверь правильность написание ФИО")
+    except MultipleObjectsReturned:
+        print(f"Уточните имя ученика, в дневнике не один {name_schoolkid} "
+              "добавь отчество или фамилию")
 
 
 def create_commendation(name_schoolkid, name_subject):
-    commendation_text = choice([
-        "Молодец!", "Отлично!", "Хорошо!",
-        "Гораздо лучше, чем я ожидал!",
-        "Ты меня приятно удивил!", "Великолепно!", "Прекрасно!",
-        "Ты меня очень обрадовал!"])
-    schoolkid = Schoolkid.objects.get(full_name__contains=name_schoolkid)
-    subject = Subject.objects.get(title__contains=name_subject,
-                                  year_of_study=6)
-    lesson_description = Lesson.objects.filter(subject=subject).order_by(
-        'date').first()
+    schoolkid = take_schoolkid(name_schoolkid)
+    try:
+        subject = Subject.objects.get(
+            title__contains=name_subject, year_of_study=schoolkid.year_of_study
+        )
+    except ObjectDoesNotExist:
+        print("Кажется допущена ошибка при заполнение данных, \n"
+              "Проверь правильность написание предмета")
+    except MultipleObjectsReturned:
+        print(f"Уточните название предмета хотели справить - {name_subject} ")
+    lesson_description = Lesson.objects.select_related(
+        "subject", "teacher").filter(subject=subject).order_by(
+        "-date").first()
     Commendation.objects.create(
         teacher=lesson_description.teacher,
-        subject=subject,
-        text=commendation_text,
+        subject=lesson_description.subject,
+        text=choice(COMMENDATION_TEXTS),
         created=lesson_description.date,
         schoolkid=schoolkid)
 
 
+def fix_marks(name_schoolkid):
+    schoolkid = take_schoolkid(name_schoolkid)
+    Mark.objects.filter(schoolkid=schoolkid).update(points=5)
+
+
 def delete_chastisement(name_schoolkid):
-    schoolkid = Schoolkid.objects.get(full_name__contains=name_schoolkid)
+    schoolkid = take_schoolkid(name_schoolkid)
     schoolkid_chastisement = Chastisement.objects.filter(schoolkid=schoolkid)
     schoolkid_chastisement.delete()
-
-
-def catch_exception(function_name, *args):
-    try:
-        function_name(*args)
-    except ObjectDoesNotExist:
-        print("Кажется допущена ошибка при заполнение данных, \n"
-              "Проверь правильность написание ФИО или Предмета.")
-    except MultipleObjectsReturned:
-        print("Уточните имя ученика и/или предмета,"
-              " и убедитесь что заполнили данные")
