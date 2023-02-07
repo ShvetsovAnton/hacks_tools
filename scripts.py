9c00cb64d3f895ed761460fbb3e374fd3a0b4c99
Anton <56098143+ShvetsovAnton@users.noreply.github.com>
2023-02-07 21:14:54 +0400

    Изменил обработку ошибок.
    Теперь except "ловим" из атрибутов модели.
    В случаи, если возникает ошибка, поднимает исключение, сообщаем об ошибке и прекращаем выполнение программы.

--- scripts.py
+++ scripts.py
@@ -1,6 +1,10 @@
 from random import choice
-from datacenter.models import *
-from django.core.exceptions import ObjectDoesNotExist, MultipleObjectsReturned
+from datacenter.models import (Schoolkid,
+                               Subject,
+                               Lesson,
+                               Commendation,
+                               Mark,
+                               Chastisement)
 
 
 COMMENDATION_TEXTS = [
@@ -14,12 +18,16 @@ def take_schoolkid(name_schoolkid):
     try:
         schoolkid = Schoolkid.objects.get(full_name__contains=name_schoolkid)
         return schoolkid
-    except ObjectDoesNotExist:
-        print("Кажется допущена ошибка при заполнение данных, \n"
-              "Проверь правильность написание ФИО")
-    except MultipleObjectsReturned:
-        print(f"Уточните имя ученика, в дневнике не один {name_schoolkid} "
-              "добавь отчество или фамилию")
+    except Schoolkid.DoesNotExist:
+        raise Schoolkid.DoesNotExist(
+            "Кажется допущена ошибка при заполнение данных, \n"
+            "проверь правильность написание ФИО"
+        )
+    except Schoolkid.MultipleObjectsReturned:
+        raise Schoolkid.MultipleObjectsReturned(
+            f"Уточните имя ученика, в дневнике не один {name_schoolkid} "
+            "добавь отчество или фамилию"
+        )
 
 
 def create_commendation(name_schoolkid, name_subject):
@@ -28,25 +36,32 @@ def create_commendation(name_schoolkid, name_subject):
         subject = Subject.objects.get(
             title__contains=name_subject, year_of_study=schoolkid.year_of_study
         )
-    except ObjectDoesNotExist:
-        print("Кажется допущена ошибка при заполнение данных, \n"
-              "Проверь правильность написание предмета")
-    except MultipleObjectsReturned:
-        print(f"Уточните название предмета хотели справить - {name_subject} ")
-    lesson_description = Lesson.objects.select_related(
-        "subject", "teacher").filter(subject=subject).order_by(
-        "-date").first()
-    Commendation.objects.create(
-        teacher=lesson_description.teacher,
-        subject=lesson_description.subject,
-        text=choice(COMMENDATION_TEXTS),
-        created=lesson_description.date,
-        schoolkid=schoolkid)
+    except Subject.DoesNotExist:
+        raise Subject.DoesNotExist(
+            "Кажется допущена ошибка при заполнение данных, \n"
+            "проверь правильность написание предмета."
+        )
+    except Subject.MultipleObjectsReturned:
+        raise Subject.MultipleObjectsReturned(
+            f"Уточните название предмета хотели справить - {name_subject} "
+        )
+    try:
+        lesson = Lesson.objects.select_related(
+            "subject", "teacher").filter(subject=subject).order_by(
+            "-date").first()
+        Commendation.objects.create(
+            teacher=lesson.teacher,
+            subject=lesson.subject,
+            text=choice(COMMENDATION_TEXTS),
+            created=lesson.date,
+            schoolkid=schoolkid)
+    except AttributeError:
+        raise print(f"Кажется по предмету {name_subject} ещё не было уроков")
 
 
 def fix_marks(name_schoolkid):
     schoolkid = take_schoolkid(name_schoolkid)
-    Mark.objects.filter(schoolkid=schoolkid).update(points=5)
+    Mark.objects.filter(schoolkid=schoolkid, points__lte=3).update(points=5)
 
 
 def delete_chastisement(name_schoolkid):
